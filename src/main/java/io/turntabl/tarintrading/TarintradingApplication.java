d0bbf5c3da06a9daa22dc087d55af943d2eff821
Joseph Sarpong <joseph-sarpong@hotmail.com>
2022-11-23 13:47:26 +0000

    TAR-3 - add initial codebase

---
+++ src/main/java/io/turntabl/tarintrading/TarintradingApplication.java
@@ -0,0 +1,13 @@
+package io.turntabl.tarintrading;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TarintradingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TarintradingApplication.class, args);
+	}
+
+}
