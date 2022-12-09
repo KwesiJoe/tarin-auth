128c77ab19dc7a443b9fa72959b4488755588fa5
Joseph Sarpong <joseph-sarpong@hotmail.com>
2022-12-09 12:57:30 +0000

    add implementation for UserDetails and UserDetailsService

---
+++ src/main/java/io/turntabl/tarintrading/security/service/UserDetailsServiceImpl.java
@@ -0,0 +1,32 @@
+package io.turntabl.tarintrading.security.service;
+
+import io.turntabl.tarintrading.model.User;
+import io.turntabl.tarintrading.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+    @Autowired
+    UserRepository userRepository;
+
+    @Override
+    @Transactional
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        User user =  userRepository.findByEmail(email)
+                .orElseThrow(() -> new UsernameNotFoundException("Email"));
+        return UserDetailsImpl.build(user);
+    }
+
+    public UserDetails loadUserByEmail(String email) throws UsernameNotFoundException {
+        User user = userRepository.findByEmail(email)
+                .orElseThrow(() -> new UsernameNotFoundException("User Not Found with username: " + email));
+
+        return UserDetailsImpl.build(user);
+    }
+
+}
