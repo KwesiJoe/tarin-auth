f85c8e7be326fef9d12ef9682d833394240e7fba
Joseph Sarpong <joseph-sarpong@hotmail.com>
2022-12-09 13:04:09 +0000

    add roles to db

--- src/main/java/io/turntabl/tarintrading/TarintradingApplication.java
+++ src/main/java/io/turntabl/tarintrading/TarintradingApplication.java
@@ -1,7 +1,12 @@
 package io.turntabl.tarintrading;
 
+import io.turntabl.tarintrading.model.ERole;
+import io.turntabl.tarintrading.model.Role;
+import io.turntabl.tarintrading.repository.RoleRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class TarintradingApplication {
@@ -10,4 +15,12 @@ public class TarintradingApplication {
 		SpringApplication.run(TarintradingApplication.class, args);
 	}
 
+	@Bean
+	CommandLineRunner commandLineRunner(RoleRepository repository) {
+		return args -> {
+			repository.save(new Role(ERole.ROLE_USER));
+			repository.save(new Role(ERole.ROLE_ADMIN));
+			repository.save(new Role(ERole.ROLE_REGULATOR));
+		};
+	}
 }
